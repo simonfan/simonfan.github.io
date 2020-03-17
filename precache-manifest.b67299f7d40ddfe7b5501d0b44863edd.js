20397467a6a3cf8c9e6846529471585b5ab33d49
Simon <simonxd@gmail.com>
2020-03-16 21:09:23 -0300

    Updates

---
+++ precache-manifest.b67299f7d40ddfe7b5501d0b44863edd.js
@@ -0,0 +1,446 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a397d3877084db77430edd2c99225b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5154331f2fb201d567",
+    "url": "/static/css/2.c4ac97a4.chunk.css"
+  },
+  {
+    "revision": "5210318b92a0cd7867bf",
+    "url": "/static/css/main.8e8a20f6.chunk.css"
+  },
+  {
+    "revision": "8e5154331f2fb201d567",
+    "url": "/static/js/2.675422d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.675422d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5210318b92a0cd7867bf",
+    "url": "/static/js/main.c1a25c43.chunk.js"
+  },
+  {
+    "revision": "6b0b2214f856dbe10e37",
+    "url": "/static/js/runtime-main.2f0edcab.js"
+  },
+  {
+    "revision": "e84e1034c3cc643db2c2787d5fa04d53",
+    "url": "/static/media/SourceCodePro-Black.e84e1034.ttf"
+  },
+  {
+    "revision": "f141a8e3419cec0737d18452f7fccdf8",
+    "url": "/static/media/SourceCodePro-Black.f141a8e3.otf"
+  },
+  {
+    "revision": "152254897e6c292b8ad0c7d65abdd645",
+    "url": "/static/media/SourceCodePro-Black.otf.15225489.woff"
+  },
+  {
+    "revision": "f56baef8e8e6dbd932e6797fb433d81d",
+    "url": "/static/media/SourceCodePro-Black.ttf.f56baef8.woff2"
+  },
+  {
+    "revision": "b4c4286f28bde6346a42b0789579bbd9",
+    "url": "/static/media/SourceCodePro-BlackIt.b4c4286f.ttf"
+  },
+  {
+    "revision": "ef61c0a4dee35652febea6e93e3ef24a",
+    "url": "/static/media/SourceCodePro-BlackIt.ef61c0a4.otf"
+  },
+  {
+    "revision": "c41d6417352c08c107db84954a0e6b81",
+    "url": "/static/media/SourceCodePro-BlackIt.otf.c41d6417.woff"
+  },
+  {
+    "revision": "6c03658445e60bf1b6a566ad2498cc0a",
+    "url": "/static/media/SourceCodePro-BlackIt.ttf.6c036584.woff2"
+  },
+  {
+    "revision": "51ada6ced5065946501d9b8244a7879f",
+    "url": "/static/media/SourceCodePro-Bold.51ada6ce.otf"
+  },
+  {
+    "revision": "a3c3082872743102af82207d10076209",
+    "url": "/static/media/SourceCodePro-Bold.a3c30828.ttf"
+  },
+  {
+    "revision": "c78ec902161e20ce8e85bfb23ef00e46",
+    "url": "/static/media/SourceCodePro-Bold.otf.c78ec902.woff"
+  },
+  {
+    "revision": "dcedf356da9362fa2c696ba39ede3a43",
+    "url": "/static/media/SourceCodePro-Bold.ttf.dcedf356.woff2"
+  },
+  {
+    "revision": "d77c7593ff187c79a73360a5edd1da75",
+    "url": "/static/media/SourceCodePro-BoldIt.d77c7593.ttf"
+  },
+  {
+    "revision": "ebb7f769a18e040ef80fa061000ca465",
+    "url": "/static/media/SourceCodePro-BoldIt.ebb7f769.otf"
+  },
+  {
+    "revision": "dc4c805b03d1ebf2ada1383b1ec1ec83",
+    "url": "/static/media/SourceCodePro-BoldIt.otf.dc4c805b.woff"
+  },
+  {
+    "revision": "950bc4260dfce76ede7d3db116036e12",
+    "url": "/static/media/SourceCodePro-BoldIt.ttf.950bc426.woff2"
+  },
+  {
+    "revision": "c23505a54557fe1136f3510e6c8ecb22",
+    "url": "/static/media/SourceCodePro-ExtraLight.c23505a5.otf"
+  },
+  {
+    "revision": "e91271c3103f76d25ede07af3fe5a33a",
+    "url": "/static/media/SourceCodePro-ExtraLight.e91271c3.ttf"
+  },
+  {
+    "revision": "2a9a3c984d315d2879042f1ba28ea33f",
+    "url": "/static/media/SourceCodePro-ExtraLight.otf.2a9a3c98.woff"
+  },
+  {
+    "revision": "6e5fd6d466a8c3a8dec63d674af66623",
+    "url": "/static/media/SourceCodePro-ExtraLight.ttf.6e5fd6d4.woff2"
+  },
+  {
+    "revision": "871d88fbe37f7feeb5b1c2f575d9cb34",
+    "url": "/static/media/SourceCodePro-ExtraLightIt.871d88fb.ttf"
+  },
+  {
+    "revision": "d89fffe99ded30ad5f46e3f6380c2913",
+    "url": "/static/media/SourceCodePro-ExtraLightIt.d89fffe9.otf"
+  },
+  {
+    "revision": "e257250436e7559bcc65ddd3fb9321ec",
+    "url": "/static/media/SourceCodePro-ExtraLightIt.otf.e2572504.woff"
+  },
+  {
+    "revision": "bffdea7012d4953623067f93764f030e",
+    "url": "/static/media/SourceCodePro-ExtraLightIt.ttf.bffdea70.woff2"
+  },
+  {
+    "revision": "13ed3db1ddcea30c57b18971e4ec8f73",
+    "url": "/static/media/SourceCodePro-It.13ed3db1.otf"
+  },
+  {
+    "revision": "4f22015ae8668226a7f7e8413fe93a81",
+    "url": "/static/media/SourceCodePro-It.4f22015a.ttf"
+  },
+  {
+    "revision": "bd9af36ed4710820c7c76c895c2a5f7f",
+    "url": "/static/media/SourceCodePro-It.otf.bd9af36e.woff"
+  },
+  {
+    "revision": "d3787f1d8e6445fcc8aca00680efafc4",
+    "url": "/static/media/SourceCodePro-It.ttf.d3787f1d.woff2"
+  },
+  {
+    "revision": "2b369146b8370ebd12b1a70005b1b492",
+    "url": "/static/media/SourceCodePro-Light.2b369146.otf"
+  },
+  {
+    "revision": "cf1c1e30cdd58629e6c71e18aac4096a",
+    "url": "/static/media/SourceCodePro-Light.cf1c1e30.ttf"
+  },
+  {
+    "revision": "7229680e1d85fd00198c8da1f2fad31f",
+    "url": "/static/media/SourceCodePro-Light.otf.7229680e.woff"
+  },
+  {
+    "revision": "10a6dcd4425f7e6bfe2c7621e323f5b5",
+    "url": "/static/media/SourceCodePro-Light.ttf.10a6dcd4.woff2"
+  },
+  {
+    "revision": "191657a5f783cfbef7b5c55246a2c0f1",
+    "url": "/static/media/SourceCodePro-LightIt.191657a5.otf"
+  },
+  {
+    "revision": "c053d3c5121a2dec272af8820a5929ad",
+    "url": "/static/media/SourceCodePro-LightIt.c053d3c5.ttf"
+  },
+  {
+    "revision": "9791048078dc8146863129ec6299c437",
+    "url": "/static/media/SourceCodePro-LightIt.otf.97910480.woff"
+  },
+  {
+    "revision": "1f0986409cfe353b12604629bac1b987",
+    "url": "/static/media/SourceCodePro-LightIt.ttf.1f098640.woff2"
+  },
+  {
+    "revision": "472c7e7a808efce9161fb8f27535bf22",
+    "url": "/static/media/SourceCodePro-Medium.472c7e7a.ttf"
+  },
+  {
+    "revision": "9db14b70aa1a4a395d56748c5d391284",
+    "url": "/static/media/SourceCodePro-Medium.9db14b70.otf"
+  },
+  {
+    "revision": "48629b50d56319a1e7456fb763115ec1",
+    "url": "/static/media/SourceCodePro-Medium.otf.48629b50.woff"
+  },
+  {
+    "revision": "8fcd3359d7bc0182dc9b0f4f63033e90",
+    "url": "/static/media/SourceCodePro-Medium.ttf.8fcd3359.woff2"
+  },
+  {
+    "revision": "4e272d3449cb55d42b6a7553bf973945",
+    "url": "/static/media/SourceCodePro-MediumIt.4e272d34.otf"
+  },
+  {
+    "revision": "7289b3252d4150c584ac2f80d80f42fc",
+    "url": "/static/media/SourceCodePro-MediumIt.7289b325.ttf"
+  },
+  {
+    "revision": "235130c28e6e70df3804bb6c4085005c",
+    "url": "/static/media/SourceCodePro-MediumIt.otf.235130c2.woff"
+  },
+  {
+    "revision": "28c695fb2461efac8386f2016b401573",
+    "url": "/static/media/SourceCodePro-MediumIt.ttf.28c695fb.woff2"
+  },
+  {
+    "revision": "1066e54d79f902a2ff4d864c9a38a183",
+    "url": "/static/media/SourceCodePro-Regular.1066e54d.ttf"
+  },
+  {
+    "revision": "20964ddec742937a7f153247edf5c09e",
+    "url": "/static/media/SourceCodePro-Regular.20964dde.otf"
+  },
+  {
+    "revision": "13ecee9da9892b550e5960d65158f356",
+    "url": "/static/media/SourceCodePro-Regular.otf.13ecee9d.woff"
+  },
+  {
+    "revision": "f43ea4c5ac35312e1fc76a6b6a2e06f0",
+    "url": "/static/media/SourceCodePro-Regular.ttf.f43ea4c5.woff2"
+  },
+  {
+    "revision": "0f54274d1c32d7255172e7feda5a2c11",
+    "url": "/static/media/SourceCodePro-Semibold.0f54274d.ttf"
+  },
+  {
+    "revision": "fd020cec8a5ae2a7210d8bf29eb6d3b4",
+    "url": "/static/media/SourceCodePro-Semibold.fd020cec.otf"
+  },
+  {
+    "revision": "dcad5590b3f5b22e6a85e7349ffdca91",
+    "url": "/static/media/SourceCodePro-Semibold.otf.dcad5590.woff"
+  },
+  {
+    "revision": "9744f877fc5b6cb47c2868e8c5340779",
+    "url": "/static/media/SourceCodePro-Semibold.ttf.9744f877.woff2"
+  },
+  {
+    "revision": "83893e6b3705db2b59c893600493a220",
+    "url": "/static/media/SourceCodePro-SemiboldIt.83893e6b.ttf"
+  },
+  {
+    "revision": "cb99819fda3f54353255e428b45cffa2",
+    "url": "/static/media/SourceCodePro-SemiboldIt.cb99819f.otf"
+  },
+  {
+    "revision": "39da30190de65becb2097f41ab824d7c",
+    "url": "/static/media/SourceCodePro-SemiboldIt.otf.39da3019.woff"
+  },
+  {
+    "revision": "b2c9b70e06203d4e286f505334c394ce",
+    "url": "/static/media/SourceCodePro-SemiboldIt.ttf.b2c9b70e.woff2"
+  },
+  {
+    "revision": "0ef9ee9f7ae99dcdff63ec103389be32",
+    "url": "/static/media/SourceSansPro-Black.0ef9ee9f.otf"
+  },
+  {
+    "revision": "4a31d5fa14f4c251809e7bcd6f954aea",
+    "url": "/static/media/SourceSansPro-Black.4a31d5fa.ttf"
+  },
+  {
+    "revision": "2c99d731f47d20365f2358feb26a2a6c",
+    "url": "/static/media/SourceSansPro-Black.otf.2c99d731.woff"
+  },
+  {
+    "revision": "1ec93fb0f9636c560e2185567ab4dccf",
+    "url": "/static/media/SourceSansPro-Black.ttf.1ec93fb0.woff2"
+  },
+  {
+    "revision": "0bf3a60fecf77ce612a02da05a9efb7d",
+    "url": "/static/media/SourceSansPro-BlackIt.0bf3a60f.otf"
+  },
+  {
+    "revision": "88c36b15c9d9d17f94cfc16347fa1457",
+    "url": "/static/media/SourceSansPro-BlackIt.88c36b15.ttf"
+  },
+  {
+    "revision": "9330ab0d245f7b6e97f9e9fd6a60b179",
+    "url": "/static/media/SourceSansPro-BlackIt.otf.9330ab0d.woff"
+  },
+  {
+    "revision": "400e7325d7cc18264f6bce3b11e0eda6",
+    "url": "/static/media/SourceSansPro-BlackIt.ttf.400e7325.woff2"
+  },
+  {
+    "revision": "b23705a4082d8c84d2533a37e9fb0811",
+    "url": "/static/media/SourceSansPro-Bold.b23705a4.otf"
+  },
+  {
+    "revision": "cd9e6b790a20bbaf25ec0027060f6c3c",
+    "url": "/static/media/SourceSansPro-Bold.cd9e6b79.ttf"
+  },
+  {
+    "revision": "ac261598a5964b71c9eaa3fe76356b4e",
+    "url": "/static/media/SourceSansPro-Bold.otf.ac261598.woff"
+  },
+  {
+    "revision": "07a207178039f72a957df5e77b061b37",
+    "url": "/static/media/SourceSansPro-Bold.ttf.07a20717.woff2"
+  },
+  {
+    "revision": "07ceebf1dec81bf92b226c6d806c21fb",
+    "url": "/static/media/SourceSansPro-BoldIt.07ceebf1.otf"
+  },
+  {
+    "revision": "7e401b3e06e481d0314c9bd1a93c9b9b",
+    "url": "/static/media/SourceSansPro-BoldIt.7e401b3e.ttf"
+  },
+  {
+    "revision": "b81c2d8b4d22ffa9b6ab1dc52843f40d",
+    "url": "/static/media/SourceSansPro-BoldIt.otf.b81c2d8b.woff"
+  },
+  {
+    "revision": "433da739918f1fbd0ff8c83e10116698",
+    "url": "/static/media/SourceSansPro-BoldIt.ttf.433da739.woff2"
+  },
+  {
+    "revision": "8d0c3b1ca9f922ff1809c1ef429910d1",
+    "url": "/static/media/SourceSansPro-ExtraLight.8d0c3b1c.ttf"
+  },
+  {
+    "revision": "c867d05d4ff064b1563c57f6a800db61",
+    "url": "/static/media/SourceSansPro-ExtraLight.c867d05d.otf"
+  },
+  {
+    "revision": "d577e2c5c6915fcefb0de4037f092e83",
+    "url": "/static/media/SourceSansPro-ExtraLight.otf.d577e2c5.woff"
+  },
+  {
+    "revision": "d92614daa93b6a308c74465b3b5e5040",
+    "url": "/static/media/SourceSansPro-ExtraLight.ttf.d92614da.woff2"
+  },
+  {
+    "revision": "5f6c950a5d5fee9299eb0fcb9d2adb0f",
+    "url": "/static/media/SourceSansPro-ExtraLightIt.5f6c950a.ttf"
+  },
+  {
+    "revision": "d6648c78d38c357e5c81c8e0a6d0aa0c",
+    "url": "/static/media/SourceSansPro-ExtraLightIt.d6648c78.otf"
+  },
+  {
+    "revision": "9b9d35be0ea84faee13c0b4f8d1e6af2",
+    "url": "/static/media/SourceSansPro-ExtraLightIt.otf.9b9d35be.woff"
+  },
+  {
+    "revision": "82097c26863b6f5a2ed9d4679e2a0e5a",
+    "url": "/static/media/SourceSansPro-ExtraLightIt.ttf.82097c26.woff2"
+  },
+  {
+    "revision": "6c362e00c9a0cd2688c041229cb6446b",
+    "url": "/static/media/SourceSansPro-It.6c362e00.ttf"
+  },
+  {
+    "revision": "ad486cdf84121217b2b4cac5b8fc7816",
+    "url": "/static/media/SourceSansPro-It.ad486cdf.otf"
+  },
+  {
+    "revision": "5f31e7ec6f0c0325ab0e5e569d2418e3",
+    "url": "/static/media/SourceSansPro-It.otf.5f31e7ec.woff"
+  },
+  {
+    "revision": "7ee03da8add1610f0a9cb55f7047dbb0",
+    "url": "/static/media/SourceSansPro-It.ttf.7ee03da8.woff2"
+  },
+  {
+    "revision": "420d839a0ccfb6a040d3a5c6af03c5aa",
+    "url": "/static/media/SourceSansPro-Light.420d839a.otf"
+  },
+  {
+    "revision": "e7daa4ab63e5209eedac1adcb98a7fed",
+    "url": "/static/media/SourceSansPro-Light.e7daa4ab.ttf"
+  },
+  {
+    "revision": "cd6456a86c3b012a26c5afe1353a3b55",
+    "url": "/static/media/SourceSansPro-Light.otf.cd6456a8.woff"
+  },
+  {
+    "revision": "7db69e930fff9eba1e8eb6fdd41c826e",
+    "url": "/static/media/SourceSansPro-Light.ttf.7db69e93.woff2"
+  },
+  {
+    "revision": "0b4be0f981068bdd05dd1c7b5933ef2d",
+    "url": "/static/media/SourceSansPro-LightIt.0b4be0f9.otf"
+  },
+  {
+    "revision": "cf7fa055e48899b14f9fbb9d060e8ad8",
+    "url": "/static/media/SourceSansPro-LightIt.cf7fa055.ttf"
+  },
+  {
+    "revision": "ed22e01b074d6e061077a1acac00a859",
+    "url": "/static/media/SourceSansPro-LightIt.otf.ed22e01b.woff"
+  },
+  {
+    "revision": "9b26dad30000c8cdd7818a7bd699cdeb",
+    "url": "/static/media/SourceSansPro-LightIt.ttf.9b26dad3.woff2"
+  },
+  {
+    "revision": "906ba80ac81b0bb01b7b2fda686c785d",
+    "url": "/static/media/SourceSansPro-Regular.906ba80a.ttf"
+  },
+  {
+    "revision": "da547242c12b7cfc5f4a4e3c428b9d76",
+    "url": "/static/media/SourceSansPro-Regular.da547242.otf"
+  },
+  {
+    "revision": "344f4c8d29dfd487ee38394adb229760",
+    "url": "/static/media/SourceSansPro-Regular.otf.344f4c8d.woff"
+  },
+  {
+    "revision": "8bb5b88b2b1b019e3b97b513b2226f3e",
+    "url": "/static/media/SourceSansPro-Regular.ttf.8bb5b88b.woff2"
+  },
+  {
+    "revision": "d22df054702cecfc67356e5dd407022f",
+    "url": "/static/media/SourceSansPro-Semibold.d22df054.otf"
+  },
+  {
+    "revision": "e8db8af2e317210f62cc1dd6527be550",
+    "url": "/static/media/SourceSansPro-Semibold.e8db8af2.ttf"
+  },
+  {
+    "revision": "90cd49254f172e2e99411daf0d574d5f",
+    "url": "/static/media/SourceSansPro-Semibold.otf.90cd4925.woff"
+  },
+  {
+    "revision": "b13c669d61ec35a7682a892456c8d4e9",
+    "url": "/static/media/SourceSansPro-Semibold.ttf.b13c669d.woff2"
+  },
+  {
+    "revision": "41995df845594d6387bb237b512e8c0e",
+    "url": "/static/media/SourceSansPro-SemiboldIt.41995df8.ttf"
+  },
+  {
+    "revision": "6de08aa7bf926c26b341cc64cf0f45bd",
+    "url": "/static/media/SourceSansPro-SemiboldIt.6de08aa7.otf"
+  },
+  {
+    "revision": "b27999d81e79da7b61cbab774097d279",
+    "url": "/static/media/SourceSansPro-SemiboldIt.otf.b27999d8.woff"
+  },
+  {
+    "revision": "c474abd9b02d9299a7944d97270357d1",
+    "url": "/static/media/SourceSansPro-SemiboldIt.ttf.c474abd9.woff2"
+  }
+]);
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './components/App/App'
-
-import 'source-code-pro/source-code-pro.css'
-import 'source-sans-pro/source-sans-pro.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
